47768387795f72de60be1cd3eda4e54793c09f8c
arrecis59 <arrecis59@gmail.com>
2018-09-19 11:41:18 -0600

    arrecis59
    uploaded

---
+++ implementacionLibreria/newk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EjemploLibreria;
+
+namespace implementacionLibreria
+{
+    public partial class newk : Form
+    {
+        Form1 fm = new Form1();
+        
+
+       //
+
+        public newk()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fm.Show();
+        }
+    }
+}
