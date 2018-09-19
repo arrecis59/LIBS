f28ed7967cd6ab4172158e8e804953b2457a014f
Valee' Castellanos. <valerycastellanos1@hotmail.com>
2018-09-19 11:44:26 -0600

    Prueba

--- implementacionLibreria/newk.Designer.cs
+++ implementacionLibreria/newk.Designer.cs
@@ -34,18 +34,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(154, 126);
+            this.label1.Location = new System.Drawing.Point(116, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Pvto el que lo lea";
             // 
             // newk
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(676, 431);
+            this.ClientSize = new System.Drawing.Size(507, 350);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "newk";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
